e62796d401a6c3d7c94544af54e8d8e005d7adc5
JimHui12 <jianghua.hui@uq.net.au>
2020-09-30 11:49:45 +1000

    updated application app component, sign-in component and sign-up component to use Hooks

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,25 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
-
 
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
-
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-class ShopPage extends React.Component {
-    state = {
-        loading: true
-    };
+const ShopPage = ({ fetchCollectionsStart, match }) => {
 
-    unsubscribeFromSnapshot = null;
-
-    componentDidMount() {
-
-        const { fetchCollectionsStart } = this.props;
+    useEffect(() => {
         fetchCollectionsStart();
+    }, [fetchCollectionsStart]);
 
         // const { updateCollections } = this.props;
         // const collectionRef = firestore.collection('collections');
@@ -43,19 +35,15 @@ class ShopPage extends React.Component {
         // fetch('https://firestore.googleapis.com/v1/projects/crown-db-e0e73/databases/(default)/documents/collections')
         // .then(response => response.json())
         // .then(collections => console.log(collections));
-    }
 
-    render() {
-        const { match } = this.props;
-        
-        return (
-            <div className="shop-page">
-                <Route exact path={`${match.path}`} component={CollectionsOverviewContainer}  />
-                <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
-            </div>
-        );
-    };
-}  
+    return (
+        <div className="shop-page">
+            <Route exact path={`${match.path}`} component={CollectionsOverviewContainer}  />
+            <Route path={`${match.path}/:collectionId`} component={CollectionPageContainer} />
+        </div>
+    );
+};
+ 
 
 const mapDispatchToProps = dispatch => ({
     fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
