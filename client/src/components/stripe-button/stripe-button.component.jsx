cbb4e29be8e58c139b0b5d7d2e31bd5a28d93ce8
JimHui12 <jianghua.hui@uq.net.au>
2020-10-02 16:22:14 +1000

    separating cra app into client folder, added express server to help handle backend stripe payment

---
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51HUMVGEb27jEtNuoF5UytSmZ6KYg9XccfbyZ8UzI90HmhZwuQLxzir0oHMafcb46bTijcN6XgfBoJqWpCXDZf3YW00mFnsITOM'
+
+    const onToken = token => {
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful');
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert(
+                'There was an issue with your payment. Please sure you use the provided credit card.'
+            )
+        })
+    };
+
+    return (
+        <StripeCheckout 
+            label="Pay Now"
+            name="CROWN CLOTHING Ltd."
+            billingAddress
+            shippingAddress
+            image="https://sendeyo.com/up/d/f3eb2117da"
+            description={`Your total is $${price}`}
+            amount={priceForStripe}
+            panelLabel="Pay Now"
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
